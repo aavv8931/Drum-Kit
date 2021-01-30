28e5ef83a94eb3fcf02ec923d695fb9d5b3f6a9d
Arturo <arturo.veintimilla@hotmail.com>
2021-01-30 10:35:27 -0500

    Buttons functionality in index JS

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("hello");
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+
+    function handleClick() {
+        alert("i got clicked");
+    }
+
+}
